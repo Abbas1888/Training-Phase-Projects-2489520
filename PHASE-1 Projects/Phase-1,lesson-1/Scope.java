65d038d03c9602afadeaf2fc5c7a09b32f0e0fbf
Abbas/2489520 <97783112+Abbas1888@users.noreply.github.com>
2022-02-27 10:21:36 +0530

    Add files via upload

---
+++ PHASE-1 Projects/Phase-1,lesson-1/Scope.java
@@ -0,0 +1,15 @@
+package com.phase1.training;
+
+import java.nio.file.AccessModifiers;
+
+public class Scope {
+
+	public static void main(String[] args) {
+		AccessModeModifiers ref=new AccessModifiers();
+		ref.flag=70;     //Public Scope
+		System.out.println(ref.flag);
+
+	}
+
+
+	}
---
+++ PHASE-1 Projects/Phase-1,lesson-1/UsingInnerClass.java
@@ -0,0 +1,54 @@
+package com.phase1.training;
+
+public class UsingInnerClass {
+
+
+
+
+		 private String msg="Welcome to Java"; 
+
+		 class Inner{  
+		  void hello(){System.out.println(msg+", Let us start learning Inner Classes");}  
+		 }  
+
+
+		public static void main(String[] args) {
+
+			UsingInnerClass obj=new UsingInnerClass();
+			UsingInnerClass.Inner in=obj.new Inner();  
+			in.hello(); 
+			 UsingInnerClasses2  ob=new UsingInnerClasses2();  
+				ob.display();  
+
+
+
+
+			//anonymous inner class
+			abstract class AnonymousInnerClass {
+				   public abstract void display();
+				}
+
+			AnonymousInnerClass i = new AnonymousInnerClass() {
+
+		         public void display() {
+		            System.out.println("Anonymous Inner Class");
+		         }
+		      };
+		      i.display();
+		}
+	}
+class  UsingInnerClasses2 {
+
+	private String msg="Inner Classes";
+
+	 void display(){  
+		 class Inner{  
+			 void msg(){
+				 System.out.println(msg);
+			 }  
+	  }  
+
+	  Inner l=new Inner();  
+	  l.msg();  
+	 }
+}
